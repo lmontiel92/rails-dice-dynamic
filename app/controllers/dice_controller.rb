2f3757481b1c20649bf3e4e8a7bef42baf47a042
lmontiel92 <146214091+lmontiel92@users.noreply.github.com>
2023-11-07 03:58:30 +0000

    initial commit

---
+++ app/controllers/dice_controller.rb
@@ -0,0 +1,19 @@
+class DiceController <ApplicationController
+  def roll_dice
+    
+    @num_dice = params.fetch("number_of_dice").to_i
+
+    @sides = params.fetch("how_many_sides").to_i
+  
+    @rolls = []
+  
+    @num_dice.times do
+      die = rand(1..@sides)
+  
+      @rolls.push(die)
+    end
+
+    render ({:template => "view_templates/results"})
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
 
+get("/dice/:number_of_dice/:how_many_sides", {:controller => "dice", :action => "roll_dice"})
+
 end
